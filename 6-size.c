5e3f858e7f10ce17d4cda5ac02c84559cfee0850
eyob6117 <eyobteklu911@gmail.com>
2021-06-18 22:01:18 -0700

    size

---
+++ 6-size.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char charType;
+int intType;
+float floatType;
+long int longType;
+long long longlongType;
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of long long: %zu byte(s)\n", sizeof(longlongType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+return (0);
+}
